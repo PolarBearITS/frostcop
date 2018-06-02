6facb2eebac155e5b5281181987828f0cddcdb17
PolarBearITS <polar.bear.in.the.snow@gmail.com>
2018-06-02 19:22:34 -0400

    basic scraping for supreme, no stock checking yet

--- monitors/monitor.py
+++ monitors/monitor.py
@@ -1,8 +1,10 @@
 import requests
+import threading
 from bs4 import BeautifulSoup as bsoup
+from requests_futures.sessions import FuturesSession
 class monitor:
 	def __init__(self):
-		pass
+		self.needsSoup
 
 	def refresh(self):
 		pass
@@ -10,5 +12,35 @@ class monitor:
 	def check_stock(self):
 		raise NotImplementedError("check_stock must be defined in child monitor class.")
 
+	def scrape(self):
+		n = len(self.links)
+		if n < 0:
+			n = 1
+		session = FuturesSession(max_workers=n)
+		reqs = ()
+		for link in self.links:
+			reqs += (session.get(link),)
+		self.responses = [r.result() for r in reqs]
+		if self.needsSoup:
+			pass
+
+		
+	def make_soup(self, response):
+		return bsoup(response.content, 'html.parser')
+
 	def run(self):
 		print(self.url)
+		print(self.needsSoup)
+		self.refresh()
+		self.scrape()
+		for resp in self.responses:
+			pass
+
+class soup:
+	def __init__(self, link):
+		self.response = requests.get(link)
+		self.soup = bsoup(self.response.content, 'html.parser')
+
+class request:
+	def __init__(self, link):
+		self.response = requests.get(link)
--- monitors/supreme/supreme.py
+++ monitors/supreme/supreme.py
@@ -1,9 +1,13 @@
 from .. import monitor
 class supreme(monitor.monitor):
-	url = 'http://www.supremenewyork.com/shop/all'
+	url = 'http://www.supremenewyork.com'
+	needsSoup = True
 
 	def refresh(self):
-		pass
+		self.links = []
+		home = monitor.soup(self.url + '/shop/all')
+		for div in home.soup.find_all('div', {'class': 'inner-article'}):
+			self.links.append(self.url + div.find('a')['href'])
 
-	def check_stock(self):
+	def check_stock(self, page):
 		pass
